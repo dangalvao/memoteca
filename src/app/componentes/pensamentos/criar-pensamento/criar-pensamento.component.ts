a81132838dc5aef5f41c3519bb657ca3a3f73e42
Danilo Galvão <danilo@infoideias.com.br>
2023-08-20 14:28:10 -0300

    Segunda aula concluida

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Angular',
+    autoria: 'Dev',
+    modelo: 'modelo3'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert('Teste')
+  }
+
+  cancelar(){
+    alert('Cancelar')
+  }
+
+}
